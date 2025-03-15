228af4d0ac32823eda7d57d2e5ee227d6b900732
Shlomi Nugarker <shlomin1231@gmail.com>
2025-03-15 14:12:43 +0200

    update mytheme: enqueue Tailwind CSS and register primary navigation menu

--- wordpress/wp-content/themes/mytheme/functions.php
+++ wordpress/wp-content/themes/mytheme/functions.php
@@ -1,15 +1,14 @@
 <?php
+// Enqueue Tailwind CSS
 function mytheme_enqueue_styles() {
     wp_enqueue_style('tailwind-style', get_template_directory_uri() . '/style.css', array(), filemtime(get_template_directory() . '/style.css'));
 }
 add_action('wp_enqueue_scripts', 'mytheme_enqueue_styles');
-?>
 
-<?php
+// Register Navigation Menus
 function mytheme_register_menus() {
     register_nav_menus(array(
         'primary' => __('Primary Menu', 'mytheme'),
     ));
 }
 add_action('after_setup_theme', 'mytheme_register_menus');
-?>
