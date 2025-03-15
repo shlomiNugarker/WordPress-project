ca934084991fd81d1045223e00781e9644866187
Shlomi Nugarker <shlomin1231@gmail.com>
2025-03-15 12:29:17 +0200

    update mytheme: register primary navigation menu and integrate it into the header with Tailwind CSS styling

--- wordpress/wp-content/themes/mytheme/functions.php
+++ wordpress/wp-content/themes/mytheme/functions.php
@@ -4,3 +4,12 @@ function mytheme_enqueue_styles() {
 }
 add_action('wp_enqueue_scripts', 'mytheme_enqueue_styles');
 ?>
+
+<?php
+function mytheme_register_menus() {
+    register_nav_menus(array(
+        'primary' => __('Primary Menu', 'mytheme'),
+    ));
+}
+add_action('after_setup_theme', 'mytheme_register_menus');
+?>
--- wordpress/wp-content/themes/mytheme/header.php
+++ wordpress/wp-content/themes/mytheme/header.php
@@ -15,9 +15,12 @@
 </header>
 
 <nav class="bg-blue-600 p-4 shadow-md">
-    <ul class="flex justify-center space-x-6">
-        <li><a href="<?php echo home_url(); ?>" class="text-white hover:underline">בית</a></li>
-        <li><a href="<?php echo site_url('/blog'); ?>" class="text-white hover:underline">בלוג</a></li>
-        <li><a href="<?php echo site_url('/contact'); ?>" class="text-white hover:underline">צור קשר</a></li>
-    </ul>
+    <?php
+    wp_nav_menu(array(
+        'theme_location' => 'primary', // מחפש תפריט תחת "Primary Menu"
+        'container' => false, // מסיר עטיפת <div> מיותרת
+        'menu_class' => 'flex justify-center space-x-6 text-white', // מוסיף עיצוב Tailwind
+        'fallback_cb' => false // מבטל תפריט ברירת מחדל אם אין תפריט מוגדר
+    ));
+    ?>
 </nav>
